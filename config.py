7ccac03739fd5160cd5d8ab2a3ffce13692e9693
RAYYAN BIN MOHD JAWEED <161345683+rayyanrbj09@users.noreply.github.com>
2025-05-10 11:44:12 +0530

    Final

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ QUESTIONS = [
     "10. Does your child stare at nothing with no apparent purpose?"
 ]
 OPTIONS = ['Always', 'Usually', 'Sometimes', 'Rarely', 'Never']
-image1 = r'static\images\asd.jpg'
+image1 = r'static\images\asd1.webp'
 image2 = r'static\images\asd.jpg'
