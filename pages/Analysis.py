b2c4a55112882b64228adade0d7882c757929c75
RAYYAN BIN MOHD JAWEED <161345683+rayyanrbj09@users.noreply.github.com>
2025-05-10 22:35:15 +0530

    MarkDown

--- pages/Analysis.py
+++ pages/Analysis.py
@@ -4,14 +4,14 @@ st.title("Analysis of Autism Spectrum Disorder")
 
 st.markdown("""
 ## From the Charts that was generated the insgihts found useful are :
-
-*12-18 Months*:
+---------------
+**12-18 Months**:
 - Predominantly light yellow cells (scores around -4 to -15) suggest lower QCHAT-10 scores, indicating fewer or less pronounced ASD traits. This aligns with the idea that autism symptoms may be subtler or harder to detect in younger toddlers.
-
-*18-24 Months*:
+---------------
+**18-24 Months**:
 - A transition zone with more orange hues (-15 to -25), reflecting an increase in higher QCHAT-10 scores. This age range is a critical window for autism screening, as many diagnostic signs (e.g., social communication delays) become more apparent.a
-
-*24-36 Months*:
+---------------
+**24-36 Months**:
 - Darker red cells (-30 to -45) dominate, indicating a higher prevalence of elevated QCHAT-10 scores. This could suggest that ASD traits are more detectable or severe in this age group, consistent with developmental progression and increased screening sensitivity.
 
 """)
